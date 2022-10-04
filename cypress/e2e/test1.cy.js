4d3358b7c8d8dc66e6473ba2f4d79762caba065b
Danijel Pleša <dplesa@inceptum.hr>
2022-10-04 02:08:51 +0200

    deleted tutorial files

--- cypress/e2e/test1.cy.js
+++
@@ -1,8 +0,0 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.get('h4').then(($els) => {
-      const texts = Array.from($els, el => el.innerText)
-    })
-    console.log(texts)
-  })
-})
